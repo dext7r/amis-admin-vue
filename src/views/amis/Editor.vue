6d4f347aceb5ba8646f4e73b5606c85105a8a005
h7ml <h7ml@qq.com>
2022-05-27 01:52:55 +0800

    feat: use vue-amis-editor

--- src/views/amis/Editor.vue
+++ src/views/amis/Editor.vue
@@ -1,161 +1,43 @@
 <template>
-  <div class="card">
-    <div class="card-header">
-      <div class="container">
-        <div class="row align-items-center">
-          <div class="col-4 text-center">
-            <div class="btn-group">
-              <div class="col-auto">
-                <label class="fs-5 col-form-label fa-brands fa-themeco"
-                  >主题样式
-                </label>
-              </div>
-              <div class="col-auto">
-                <button
-                  aria-expanded="false"
-                  class="btn btn-primary dropdown-toggle ms-3"
-                  data-bs-toggle="dropdown"
-                  type="button"
-                >
-                  {{ editor.theme }}
-                </button>
-                <ul class="dropdown-menu">
-                  <li>
-                    <a class="dropdown-item" @click="changeTheme('default')"
-                      >default</a
-                    >
-                  </li>
-                  <li>
-                    <a class="dropdown-item" @click="changeTheme('cxd')">cxd</a>
-                  </li>
-                  <li>
-                    <a class="dropdown-item" @click="changeTheme('dark')"
-                      >dark</a
-                    >
-                  </li>
-                  <li>
-                    <a class="dropdown-item" @click="changeTheme('antd')"
-                      >antd</a
-                    >
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
-          <div class="col-4 text-center">
-            <div class="btn-group">
-              <div class="col-auto">
-                <label
-                  class="fs-5 col-form-label"
-                  :class="
-                    editor.isMobile
-                      ? 'fa-duotone fa-phone'
-                      : 'fa-brands fa-windows'
-                  "
-                >
-                  {{ editor.isMobile ? '移动端' : 'PC端' }}
-                </label>
-              </div>
-              <button
-                aria-expanded="false"
-                class="btn btn-primary dropdown-toggle ms-3"
-                :class="
-                  editor.isMobile
-                    ? 'fa-duotone fa-phone'
-                    : 'fa-brands fa-windows'
-                "
-                data-bs-toggle="dropdown"
-                type="button"
-              >
-                {{ editor.isMobile ? '移动端' : 'PC端' }}
-              </button>
-              <ul class="dropdown-menu">
-                <li>
-                  <a
-                    class="dropdown-item fa-brands fa-windows"
-                    @click="changeMobile(false)"
-                    >PC端</a
-                  >
-                </li>
-                <li>
-                  <a
-                    class="dropdown-item fa-duotone fa-phone"
-                    @click="changeMobile(true)"
-                    >移动端</a
-                  >
-                </li>
-              </ul>
-            </div>
-          </div>
-          <div class="col-4 text-center">
-            <div class="btn-group">
-              <div class="col-auto">
-                <label
-                  class="fs-5 col-form-label fa-solid"
-                  :class="editor.isPreview ? 'fa-eye' : 'fa-pen-to-square'"
-                >
-                  {{ editor.isPreview ? '预览' : '编辑' }}
-                </label>
-              </div>
-              <button
-                aria-expanded="false"
-                class="btn btn-primary dropdown-toggle ms-3"
-                data-bs-toggle="dropdown"
-                type="button"
-              >
-                {{ editor.isPreview ? '预览' : '编辑' }}
-              </button>
-              <ul class="dropdown-menu">
-                <li>
-                  <a
-                    class="dropdown-item fa-solid fa-eye"
-                    @click="changePreview(true)"
-                    >预览</a
-                  >
-                </li>
-                <li>
-                  <a
-                    class="dropdown-item fa-solid fa-pen-to-square"
-                    @click="changePreview(false)"
-                    >编辑</a
-                  >
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="card-body">
-      <AmisEditor
-        :is-mobile="editor.isMobile"
-        :is-preview="editor.isPreview"
-        :theme="editor.theme"
-      />
-    </div>
-  </div>
+  <vue-amis-editor :is-preview="preview" :value="schema" />
 </template>
 
 <script>
-import '@fortawesome/fontawesome-free/css/all.css'
-import { mapGetters, mapMutations } from 'vuex'
-import AmisEditor from '@/components/Editor/index.vue'
+import VueAmisEditor from 'vue-amis-editor'
 export default {
   name: 'Editor',
   components: {
-    AmisEditor
+    VueAmisEditor
   },
-  computed: {
-    ...mapGetters({
-      editor: 'amis/editor'
-    })
+  data() {
+    return {
+      preview: false,
+      schema: {
+        type: 'tasks',
+        name: 'tasks',
+        items: [
+          {
+            label: 'hive 任务',
+            key: 'hive',
+            status: 4,
+            remark:
+              '查看详情<a target="_blank" href="http://www.baidu.com">日志</a>。'
+          },
+          {
+            label: '小流量',
+            key: 'partial',
+            status: 4
+          },
+          {
+            label: '全量',
+            key: 'full',
+            status: 4
+          }
+        ],
+        id: 'u:5f649ab86d6a'
+      }
+    }
   },
-  methods: {
-    ...mapMutations({
-      changeMobile: 'amis/changeMobile',
-      changePreview: 'amis/changePreview',
-      changeTheme: 'amis/changeTheme'
-    })
-  }
+  methods: {}
 }
 </script>
