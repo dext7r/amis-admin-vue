64e006aa009fab7d91820fca27df02953e34b1f1
h7ml <h7ml@users.noreply.github.com>
2022-10-08 14:57:08 +0800

    Deploying to gh-pages from @ h7ml/amis-admin-vue@b377b3ae1759e2cd0e9b3b5d6f41908d47d3f767 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/amis-admin-vue/precache-manifest.2454dc6905f1ae59fdbd8a1a51ef0d1e.js"
+  "/amis-admin-vue/precache-manifest.55cd6753f5e9ebf2f65ff861eda9c356.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "amis-admin-vue"});
